f2f6b330a512d59ba2cee46dd70f523369b60eec
Jose Luis Churo Vicente <josheluis96@gmail.com>
2026-03-18 02:05:15 -0500

    refactor: remove film-tree component from film-card

--- src/app/features/films/components/film-card/film-card.ts
+++ src/app/features/films/components/film-card/film-card.ts
@@ -1,18 +1,13 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Film } from '../../../../core/models/film.model';
 import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
-import { FilmTree } from '../film-tree/film-tree';
-import { TreeNode } from 'primeng/api';
-import { TreeModule } from 'primeng/tree';
 
 @Component({
   selector: 'app-film-card',
-  imports: [CardModule, ButtonModule, FilmTree],
+  imports: [CardModule],
   templateUrl: './film-card.html',
   styleUrl: './film-card.scss',
 })
 export class FilmCard {
   film = input.required<Film>();
-
 }
